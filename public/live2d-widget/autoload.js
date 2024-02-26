f58ddafb383a77673ae301ff356a9222bc4f1b3b
liangjinhai <fateking27@outlook.com>
2024-02-26 10:49:07 +0800

    live2d

--- public/live2d-widget/autoload.js
+++ public/live2d-widget/autoload.js
@@ -27,9 +27,9 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
 	Promise.all([
-		loadExternalResource(live2d_path + "waifu.css", "css"),
-		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
+		loadExternalResource("./waifu.css", "css"),
+		loadExternalResource("./live2d.min.js", "js"),
+		loadExternalResource("./waifu-tips.js", "js")
 	]).then(() => {
 		// 配置选项的具体用法见 README.md
 		initWidget({
