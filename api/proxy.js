37b1cb1e445a87c92e202e83512c5c68bcf5bf23
liangjinhai <fateking27@outlook.com>
2024-04-29 11:28:15 +0800

    fix_proxy_01

--- api/proxy.js
+++ api/proxy.js
@@ -15,7 +15,7 @@ module.exports = (req, res) => {
         changeOrigin: true,
         pathRewrite: {
             // 通过路径重写，去除请求路径中的 `/bgmApi`
-            '/^\/bgmApi/': '/'
+            '^/bgmApi/': '/'
         }
     })(req, res)
 }
